f1da2e53234297c005b5ee2d56ee80ef04dc6d66
Tanvii17 <tanvihdevi@gmail.com>
2021-06-24 14:10:04 +0000

    all

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function compute()
 {
     var principal = document.getElementById("principal").value;
+    
     if(principal == "" || principal <= 0)
     {
         alert("Enter a positive number");
@@ -12,7 +13,7 @@ function compute()
     var interest = principal * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years);
     document.getElementById("result").innerHTML="If you deposit "+"<span class='highlight'>"+principal+"</span>"+",\<br\>at an interest rate of "+"<span class='highlight'>"+rate+"</span>"+"%\<br\>You will receive an amount of "+"<span class='highlight'>"+interest+"</span>"+",\<br\>in the year "+"<span class='highlight'>"+year+"</span>"+"\<br\>"
-     
+   
 }
 function updateRate() 
 {
@@ -29,4 +30,3 @@ function highlight(text) {
      inputText.innerHTML = innerHTML;
     }
   }
-        
